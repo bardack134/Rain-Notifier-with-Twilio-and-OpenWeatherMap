43f7aaf5d43747aa1ac05a332b85e45a9553ce04
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-03-28 17:39:09 +0900

    "cnt":5

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import requests
+from constants import*
 
-
-API_KEY="a4268efc4142f9eb0c24aecdb6f90603"
-MY_LATITUDE=43.057455
-MY_LONGITUDE=43.057455
 
 
 # #parametros que pasare a la api, en formatio diccionario
 weather_parameters={"lat":MY_LATITUDE,
             "lon":MY_LONGITUDE,
             "appid":API_KEY,
-            "units":"metric"
+            "units":"metric",
+            "cnt":5 #para los primeros 5 chequeos del clima,  1 dia tiene 8 chequeos, en total son 40 chequeos que son en total 5 dias
             }
 
 
@@ -24,6 +22,10 @@ response=requests.get(weather_api, params=weather_parameters)
 
 #imprimimos la http response code, un numero que indica el status (200 is ok....etc)
 print(response.status_code)
+
+
+# returns an HTTPError object if an error has occurred during the process.
+print(response.raise_for_status())
 
 
 #convertimos la respuesta tipo formaqto json
